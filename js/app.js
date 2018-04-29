5864e9a2c5f60a39e0f777a70ff9445800efe910
Virgil-Avram <avraam.virgil@yahoo.com>
2018-04-29 10:57:29 +0200

    Declaring global variables in the app.js file

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// Declaring global variables
+const card = document.querySelectorAll(".card");
+const cardList = Array.from(card);
+const deck = document.querySelector(".deck");
+let moves = 0;
+let movesCounter = document.querySelector(".moves");
+let scorePanel = document.querySelector(".score-panel");
+let starList = document.querySelector(".stars");
+let stars = starList.querySelectorAll("li");
+const reloadBtn = document.querySelector(".restart");
+const gameContainer = document.querySelector(".container");
+const modal = document.querySelector(".modal-background");
+const closeModal = document.querySelector(".close-modal");
+let endMoves = document.querySelector(".end-moves");
+let timeEnd = document.querySelector(".time-end");
+let finalRating = document.querySelector(".final-rating");
+const replayBtn = document.querySelector(".replay-game");
+let timer = document.querySelector(".timer");
+let seconds = 0;
+let minutes = 0;
+let clock;
+let opendCards = [];
+let matchedCards = [];
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -24,15 +36,3 @@ function shuffle(array) {
 
     return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
